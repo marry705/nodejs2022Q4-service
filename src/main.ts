0df8bbe78822d5b9f0353e55f0041632bd517faf
marina.shilo <marina.shilo@degree53.com>
2023-02-01 22:24:31 +0300

    feat: add env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { env } from 'process';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const DEFAULT_PORT = '4000';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(env.MAIN_PORT ?? DEFAULT_PORT);
 }
 bootstrap();
